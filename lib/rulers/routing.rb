0799d3206a783fb1307696ac3c84ae0ed74c8be9
lvsong <lvsong@rd.tuan800.com>
2014-09-21 21:26:46 +0800

    增加对favicon.icon的请求处理

--- lib/rulers/routing.rb
+++ lib/rulers/routing.rb
@@ -1,6 +1,10 @@
 module Rulers
   class Application
     def get_controller_and_action(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404,
+          {'Content-Type' => 'text/html'}, []]
+      end
       _, cont, action, after = 
         env["PATH_INFO"].split('/', 4)
       cont = cont.capitalize
