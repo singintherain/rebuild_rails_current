6b74725174153ed1041c7720d097d16c3c12fe58
lvsong <lvsong@rd.tuan800.com>
2014-09-21 20:06:36 +0800

    生成brick-0.0.3

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,13 +1,25 @@
 require "rulers/version"
-require 'rulers/array'
+require 'rulers/routing'
 
 module Rulers
   # Your code goes here...
   class Application
     def call(env)
-      `echo debug > debug.txt`;
+      klass ,act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text      = controller.send(act)
       [200, {'Content-Type' => 'text/html'},
-        ['Hello from Ruby on Rulers']]
+       [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
